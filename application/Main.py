ae00028e68e5679ac7bf2476a8cdcb85f73ba7f0
Serg86 <129606991+serg-hka@users.noreply.github.com>
2023-12-10 17:42:51 +0100

    Main Prototyp

---
+++ application/Main.py
@@ -0,0 +1,40 @@
+# #include "Arduino.h"
+
+# #include "Microcontroller.h"
+import Microcontroller
+# #include "display.h"
+import Display
+# #include "CoinDetection.h"
+import CoinDetection
+# #include "Joystick.h"
+import Joystick
+
+# #include <Wire.h>
+
+
+totalCoinValue: int
+currentCoinValue: int
+
+
+# TODO C++: Display* display;
+display: Display
+
+
+def setup():
+    pass
+
+
+def loop():
+    pass
+
+
+def ca_coinDetected(coinType: CoinDetection.coin):
+    pass
+
+
+def checkCoinValues():
+    pass
+
+
+def checkScoreAndPlaySound():
+    pass
