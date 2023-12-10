1215fba5a92ef775ebc414dd42da93975b4337ca
Serg86 <129606991+serg-hka@users.noreply.github.com>
2023-12-10 17:43:30 +0100

    SpaceSounds Prototyp

---
+++ application/SpaceSounds.py
@@ -0,0 +1,15 @@
+class SpaceSounds:
+    def initialize(self):
+        pass
+
+    def playSoundShipShot(self):
+        pass
+
+    def playSoundShipMove(self):
+        pass
+
+    def playSoundShipHasBeenHit(self):
+        pass
+
+    def playSoundProjectileCollision(self):
+        pass
