fbff534cb9848b0d4db8c0f27d209c44d006fed5
Serg86 <129606991+serg-hka@users.noreply.github.com>
2023-12-10 17:41:34 +0100

    CurveFeverView Prototyp

---
+++ application/CurveFeverView.py
@@ -0,0 +1,41 @@
+# #include "Arduino.h"
+# #include "CurveFeverLogic.h"
+from CurveFeverLogic import CellUpdate
+
+# TODO C++: #define EMPTY_COLOR Display::getColor(0, 0, 0);
+EMPTY_COLOR: int = Display.getColor(0, 0, 0) # Display::getColor(0, 0, 0);
+PLAYER_1_COLOR_1: int = Display.getColor(7, 0, 0) # Display::getColor(7, 0, 0);
+PLAYER_1_COLOR_2: int = Display.getColor(4, 0, 0) # Display::getColor(4, 0, 0);
+PLAYER_2_COLOR_1: int = Display.getColor(0, 7, 0) # Display::getColor(0, 7, 0);
+PLAYER_2_COLOR_2: int = Display.getColor(0, 4, 0) # Display::getColor(0, 4, 0);
+ITEM_1_COLOR: int = Display.getColor(0, 0, 7) # Display::getColor(0, 0, 7);
+ITEM_2_COLOR: int = Display.getColor(0, 2, 2) # Display::getColor(0, 2, 2);
+ITEM_3_COLOR: int = Display.getColor(3, 3, 3) # Display::getColor(3, 3, 3);
+
+
+class CurveFeverView:
+    def __getColorOfState(state: int) -> int:
+        pass
+
+    # TODO C++: void printGameName(char *name);
+    def printGameName(self, name: int):
+        pass
+
+    # TODO C++: void updatePixels(CellUpdate* cells, uint8_t cellCount);
+    def updatePixels(self, cells: CellUpdate, cellCount: int):
+        pass
+
+    def clearScreen(self):
+        pass
+
+    def setBoost(self, player: int, boost: int):
+        pass
+
+    def setTime(self, time: int):
+        pass
+
+    def printWinner(self, winner: int):
+        pass
+
+    def playSound(self, soundId: int):
+        pass
