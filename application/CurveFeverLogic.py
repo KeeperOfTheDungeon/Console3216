09190713135fcbf27971f5f3b4ec9df0cdfa9a4a
Serg86 <129606991+serg-hka@users.noreply.github.com>
2023-12-10 17:41:15 +0100

    CurveFeverLogic Prototyp

---
+++ application/CurveFeverLogic.py
@@ -0,0 +1,125 @@
+# #include "CurveFeverPlayer.h"
+from CurveFeverPlayer import Item, Player, Position
+# #include "Joystick.h"
+import Joystick
+
+FIELD_HEIGHT = 16
+FIELD_WIDTH = 32
+FIELD_SIZE = 4 * FIELD_HEIGHT * FIELD_WIDTH / 8
+MAX_CHANGE_COUNT = 5
+
+PLAYER_1 = 0
+PLAYER_2 = 1
+DRAW = 2
+
+NO_SOUND = 0
+SOUND_BACKGROUND = 1
+SOUND_ITEM_COLLECT = 2
+SOUND_ITEM_ACTIVATE = 3
+SOUND_GAMEOVER = 4
+
+STATE_EMPTY = 0
+STATE_PLAYER_1 = 1
+STATE_PLAYER_2 = 2
+STATE_ITEM_SLOW = 3
+STATE_ITEM_WALL = 4
+STATE_ITEM_CLEAR = 5
+STATE_TAIL_1 = 6
+STATE_TAIL_2 = 7
+
+PROBABILITY_ITEM_SLOW = 16
+PROBABILITY_ITEM_WALL = 8
+PROBABILITY_ITEM_CLEAR = 4
+
+STATE_ITEM_1 = 3
+STATE_ITEM_2 = 4
+STATE_ITEM_3 = 5
+
+
+# TODO C++: typedef struct {...} CellUpdate;
+class CellUpdate:
+    def __init__(self):
+        self.x: int
+        self.y: int
+        self.state: int
+
+
+# TODO C++: typedef struct {...} GameStatus;
+class GameStatus:
+    def __init__(self):
+        self.gameOver: bool
+        self.clearScreen: bool
+        self.cellUpdates: CellUpdate  # TODO C++: CellUpdate* cellUpdates;
+        self.updateCount: int
+        self.soundStatus: int
+        self.player1Boost: int
+        self.player2Boost: int
+
+
+class Logic:
+
+    def __init__(self):
+        # TODO C++: Joystick* joystick1;
+        self.__joystick1: Joystick
+        # TODO C++: Joystick* joystick2;
+        self.__joystick2: Joystick
+        # TODO C++: CellUpdate cellUpdates[MAX_CHANGE_COUNT];
+        self.__cellUpdates: CellUpdate = [] * MAX_CHANGE_COUNT
+
+        self.__status: GameStatus
+        self.__player1: Player
+        self.__player2: Player
+        self.__winner: int
+        self.__currentItem: Item
+        self.__itemPosition: Position
+        self.__gameTicks: int
+
+        # TODO C++: uint8_t field[FIELD_WIDTH][FIELD_HEIGHT];
+        self.__field: int = [[] * FIELD_WIDTH] * FIELD_HEIGHT
+
+    def __generateItem(self) -> int:
+        pass
+
+    def __getCellState(self, x: int, y: int) -> int:
+        pass
+
+    def __setCellState(self, x: int, y: int, state: int, publish: bool=False):
+        pass
+    
+    def __clearField(self):
+        pass
+
+    def __setDirectionForPlayer(self, player: Player):
+        pass
+
+    def __updateOccursThisTick(self, speed: int) -> bool:
+        pass
+
+    def __updateCell(self, player: Player, head: bool):
+        pass
+
+    def __setWinnerAfterValidMove(self, player1Updated: bool, player2Updated: bool):
+        pass
+
+    def __maybeGenerateItem(self):
+        pass
+
+    def __updatePlayer(self, player: Player, joystick: Joystick) -> bool:
+        pass
+
+    def __maybeAbsorbItem(self):
+        pass
+
+    def __havePlayersCollided(self, pos1: Position, pos2: Position, dir1: int, dir2: int) -> bool:
+        pass
+
+    # TODO C++ return Pointer
+    def initGame(self, p1_joystick: Joystick, p2_joystick: Joystick) -> GameStatus:
+        pass
+
+    # TODO C++ return Pointer
+    def move(self):
+        pass
+
+    def getWinner(self) -> int:
+        pass
