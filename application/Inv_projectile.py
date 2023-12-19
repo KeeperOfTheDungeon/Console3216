2d8a395a1da2d5349c976ff2c7dbee3c8e75a2a6
Serg86 <129606991+serg-hka@users.noreply.github.com>
2023-12-19 19:49:16 +0100

    implemented inv_projectile

--- application/Inv_projectile.py
+++ application/Inv_projectile.py
@@ -2,16 +2,28 @@
 # #include "sprite.h"
 import Sprite
 
+import Display
 
 class Projectile(Sprite):
     def __init__(self):
+        super().__init__(0, 0, 1, 1)
         self._vectorY: int
 
         self._movementPrescaler: int
         self._active: bool
 
+        self._bitmap[0] = Display.getColor(4, 7, 0)
+
     def move(self):
+        if self._yPos > 0 and self._yPos < 16:
+            self._yPos += self._vectorY
+        else:
+            self.deActivate()
         pass
 
     def setDirection(self, direction: bool):
+        if direction:
+            self._vectorY = 1
+        else:
+            self._vectorY = -1
         pass
