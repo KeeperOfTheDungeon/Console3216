740a196bc6128d7a95e8529e791c0ec0aad97d49
Serg86 <129606991+serg-hka@users.noreply.github.com>
2024-01-07 10:44:09 +0100

    constructor + move func implemented

--- application/Inv_ufo.py
+++ application/Inv_ufo.py
@@ -4,6 +4,7 @@ import Sprite
 # #include "Joystick.h"
 import Joystick
 
+import Display
 
 PILOT_TRIGGER_UP = 1
 PILOT_TRIGGER_BODY = 2
@@ -11,9 +12,20 @@ PILOT_TRIGGER_BODY = 2
 
 class Ufo(Sprite):
     def __init__(self):
+        # TODO Super constructor call
+        super().__init__(0, 0, 1, 1)
+
         self._movementPrescaler: int
 
+        # TODO GetColor
+        self._bitmap[0] = Display.getColor(7, 7, 7)
+        self.activate()
+        self._yPos = 0
+
     def move(self):
+        self._xPos += 1
+        if self._xPos > 31:
+            self.deActivate()
         pass
 
     # Im Original mit Schreibfehler
