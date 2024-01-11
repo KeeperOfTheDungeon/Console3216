4212d76d06b0dd3d497582a4173f74cafc726e7d
Serg86 <129606991+serg-hka@users.noreply.github.com>
2024-01-11 21:01:32 +0100

    constructor/init implemented import fixes

--- application/TetrisPlayer.py
+++ application/TetrisPlayer.py
@@ -6,6 +6,7 @@ import Game
 # #include "inc/TetrisTetrom.h"
 import TetrisTetrom
 
+import Display
 
 class TetrisPlayerStatus(Enum):
     PLAYING = 0
@@ -18,26 +19,28 @@ class TetrisPlayer:
 
         self.__status: TetrisPlayerStatus
         # TODO C++: TetrisTetrom* currentTetrom;
-        self.__currentTetrom: TetrisTetrom
+        self.__currentTetrom: TetrisTetrom.TetrisTetrom
         # TODO C++: TetrisTetrom* nextTetrom;
-        self.__nextTetrom: TetrisTetrom
+        self.__nextTetrom: TetrisTetrom.TetrisTetrom
         self.__deltaSum: int
         self.__updateDelay: int
         self.__mapColor: int
-        self.__mapOffsetX: int
-        self.__mapOffsetY: int
+        self.__mapOffsetX: int = offsetX
+        self.__mapOffsetY: int = offsetY
         self.__softDropDelay: int = 50
         self.__normalDelay: int = 200
 
         # TODO C++: uint8_t map[16][10] = {...};
-        self.__map: int = [[0] * 10] * 16
+        self.__map: int = [[0 for _ in range(10)] for _ in range(16)]
 
         self.playerpoints: int
 
-    def __canMoveTetrom(self, tetrom: TetrisTetrom, xOffset: int, yOffset: int) -> bool:
+        self.init()
+
+    def __canMoveTetrom(self, tetrom: TetrisTetrom.TetrisTetrom, xOffset: int, yOffset: int) -> bool:
         pass
 
-    def __canRotateTetrom(self, tetrom: TetrisTetrom, clockwise: bool) -> bool:
+    def __canRotateTetrom(self, tetrom: TetrisTetrom.TetrisTetrom, clockwise: bool) -> bool:
         pass
 
     def __initializeTetroms(self):
@@ -46,28 +49,28 @@ class TetrisPlayer:
     def __assignNextTetroms(self):
         pass
 
-    def __generateTetrom(self) -> TetrisTetrom:
+    def __generateTetrom(self) -> TetrisTetrom.TetrisTetrom:
         pass
 
     def __logicalUpdate(self, delta: int):
         pass
 
-    def __hasCollisions(self, tetrom: TetrisTetrom) -> bool:
+    def __hasCollisions(self, tetrom: TetrisTetrom.TetrisTetrom) -> bool:
         pass
 
     def __mapEmpty(self, x: int, y: int) -> bool:
         pass
 
-    def __placeOnMap(self, tetrom: TetrisTetrom):
+    def __placeOnMap(self, tetrom: TetrisTetrom.TetrisTetrom):
         pass
 
     def __changeMap(self, x: int, y: int, value: int):
         pass
 
-    def __moveIfPossible(self, tetrom: TetrisTetrom, xOffset: int, yOffset: int):
+    def __moveIfPossible(self, tetrom: TetrisTetrom.TetrisTetrom, xOffset: int, yOffset: int):
         pass
 
-    def __rotateIfPossible(self, tetrom: TetrisTetrom, clockwise: bool):
+    def __rotateIfPossible(self, tetrom: TetrisTetrom.TetrisTetrom, clockwise: bool):
         pass
 
     def returnPlayerPoints(self) -> int:
@@ -77,6 +80,14 @@ class TetrisPlayer:
         pass
 
     def init(self):
+        self.playerpoints = 0
+        self.__status = TetrisPlayerStatus.PLAYING
+        self.__mapColor = Display.Display.getColor(2, 0, 2)
+        self.__temporaryCounter = 0
+        self.__deltaSum = 0
+        self.__updateDelay = self.__normalDelay
+
+        self.__initializeTetroms()
         pass
 
     def reset(self):
@@ -94,10 +105,10 @@ class TetrisPlayer:
     def update(self, delta: int):
         pass
 
-    def getCurrentTetrom(self) -> TetrisTetrom:
+    def getCurrentTetrom(self) -> TetrisTetrom.TetrisTetrom:
         pass
 
-    def getNextTetrom(self) -> TetrisTetrom:
+    def getNextTetrom(self) -> TetrisTetrom.TetrisTetrom:
         pass
 
     def softDropOn(self):
