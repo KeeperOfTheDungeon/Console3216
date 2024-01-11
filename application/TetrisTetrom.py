eebd2c438a7150cf5825e9e053c48be2efe6c4a2
Serg86 <129606991+serg-hka@users.noreply.github.com>
2024-01-11 22:12:24 +0100

    file implemented

--- application/TetrisTetrom.py
+++ application/TetrisTetrom.py
@@ -2,6 +2,8 @@
 # #include "game.h"
 import Game
 
+import Display
+
 # TODO C++: static const uint8_t tetromRotations[7][4][4][4] = {...}
 tetromRotations: int = [
     # J 0
@@ -127,56 +129,73 @@ tetromRotations: int = [
 
 class TetrisTetrom:
     def __init__(self, offsetX: int, offsetY: int):
-        self.__mapOffsetX: int
-        self.__mapOffsetY: int
-        self.__positionX: int
-        self.__positionY: int
-        self.__rotationIndex: int
-        self.__tetromIndex: int
-        self.__color: int
+        self.__mapOffsetX: int = offsetX
+        self.__mapOffsetY: int = offsetY
+        self.__positionX: int = 0
+        self.__positionY: int = 0
+        self.__rotationIndex: int = 0
+        self.__tetromIndex: int = 0
+        self.__color: int = Display.Display.getColor(2, 0, 2)
         pass
 
     def getX(self) -> int:
-        pass
+        return self.__positionX
 
     def getY(self) -> int:
-        pass
+        return self.__positionY
 
     def moveRelative(self, x: int, y: int):
+        self.__positionX += x
+        self.__positionY += y
         pass
 
     def setPosition(self, x: int, y: int):
+        self.__positionX = x
+        self.__positionY = y
         pass
 
     def rotateLeft(self):
+        self.__rotationIndex -= 1
+        self.__rotationIndex %= 4
         pass
 
     def rotateRight(self):
+        self.__rotationIndex += 1
+        self.__rotationIndex %= 4
         pass
 
     def setRotation(self, rotation: int):
+        self.__rotationIndex = rotation
         pass
 
     def getRotation(self) -> int:
-        pass
+        return self.__rotationIndex
 
     def setType(self, type: int):
+        self.__tetromIndex = type
         pass
 
     def getType(self) -> int:
-        pass
+        return self.__tetromIndex
 
     """
     * Always 4x4 [y][x]
     """
     def getPixel(self, x: int, y: int) -> int:
-        pass
+        return tetromRotations[self.__tetromIndex][self.__rotationIndex][x][y]
 
     def getCollision(self, x: int, y: int) -> bool:
-        pass
+        return self.getPixel(x, y) != 0
 
     def setColor(self, color: int):
+        self.__color = color
         pass
 
     def draw(self):
+        for x in range(4):
+            for y in range(4):
+                if self.getPixel(x, y) != 0:
+                    calcX: int = x + self.__mapOffsetX + self.__positionX
+                    calcY: int = y + self.__mapOffsetY + self.__positionY
+                    Display.Display.drawPixel(calcX, calcY, self.__color)
         pass
