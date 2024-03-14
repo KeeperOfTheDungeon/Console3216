d2b712f724136e4f978a27a0efa3b123f0cb90ba
Serg86 <129606991+serg-hka@users.noreply.github.com>
2024-03-14 10:35:13 +0100

    Syntax fix, Methodenaufrufe angepasst

--- application/Inv_projectile.py
+++ application/Inv_projectile.py
@@ -7,12 +7,12 @@ import Display
 class Projectile(Sprite.Sprite):
     def __init__(self):
         super().__init__(0, 0, 1, 1)
-        self._vectorY: int
+        self._vectorY: int = 0
 
-        self._movementPrescaler: int
-        self._active: bool
+        self._movementPrescaler: int = 0
+        self._active: bool = False
 
-        self._bitmap[0] = Display.Display.getColor(4, 7, 0)
+        self._bitmap[0] = Display.Display.getColorFrom333(4, 7, 0)
 
     def move(self):
         if self._yPos > 0 and self._yPos < 16:
